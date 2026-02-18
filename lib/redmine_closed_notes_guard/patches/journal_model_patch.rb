6337796b995e073e7ecaef2056b3c9c9a7b685f8
Zsolt Szabó-Pap <72798545+zsoltrego@users.noreply.github.com>
2026-02-18 14:38:14 +0100

    Add validation to block notes on closed issues

---
+++ lib/redmine_closed_notes_guard/patches/journal_model_patch.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module RedmineClosedNotesGuard
+  module Patches
+    module JournalModelPatch
+      def self.included(base)
+        base.class_eval do
+          validate :rcng_block_notes_on_closed_issue_for_roles
+        end
+      end
+
+      private
+
+      def rcng_block_notes_on_closed_issue_for_roles
+        # Only care about journals on issues
+        issue = journalized.is_a?(Issue) ? journalized : nil
+        return unless issue
+
+        # Only when issue is closed
+        return unless issue.closed?
+
+        # Only when notes are present (adding/editing a comment)
+        return unless notes.present?
+
+        blocked_ids = Array(Setting.plugin_redmine_closed_notes_guard['blocked_role_ids']).map(&:to_i)
+        return if blocked_ids.empty?
+
+        user = User.current
+        return if user.nil? || user.anonymous?
+
+        user_role_ids = user.roles_for_project(issue.project).map(&:id)
+
+        # If user has any blocked role -> forbid
+        return if (user_role_ids & blocked_ids).empty?
+
+        errors.add(:notes, :rcng_notes_forbidden_on_closed)
+      end
+    end
+  end
+end
+
+unless Journal.included_modules.include?(RedmineClosedNotesGuard::Patches::JournalModelPatch)
+  Journal.include RedmineClosedNotesGuard::Patches::JournalModelPatch
+end
